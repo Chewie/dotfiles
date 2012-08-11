d52d35eb69eeeec93fba90fdc3548ef7188a46c5
Kévin Sztern <sztern.kevin@gmail.com>
2012-08-11 11:43:43 +0200

    Updated install script

--- install.zsh
+++ install.zsh
@@ -1,14 +1,18 @@
 #! /bin/zsh
 
 ## Warning: this script assumes that you are using zsh and running it from
-## its directory (ie : ./install.sh)
+## its directory (ie : ./install.zsh)
 ## This is a quick and dirty script, don't expect anything fancy from it
 
+
+# Set options related to globbing, for the last line
 setopt extendedglob
 setopt glob_dots
 
+# Synchronize the git submodules
 git submodule init
 git submodule update
-cd $HOME
 
-ln -s $OLDPWD/*~*.git~*install.sh .
+# Create symlinks
+cd $HOME
+ln -s $OLDPWD/*~*.git~*install.sh~*README.markdown .
