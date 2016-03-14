8cb11ae3ed5d335abc098d9b1943feb71f9b1a9c
Kévin Sztern <sztern.kevin@gmail.com>
2016-03-14 15:31:59 +0100

    Move dotfiles management to stow

--- install.zsh
+++ install.zsh
@@ -1,8 +1,4 @@
-#! /bin/zsh
-
-## Warning: this script assumes that you are using zsh and running it from
-## its directory (ie : ./install.zsh)
-## This is a quick and dirty script, don't expect anything fancy from it
+#! /bin/sh
 
 
 # Set options related to globbing, for the last line
@@ -13,6 +9,11 @@ setopt glob_dots
 git submodule init
 git submodule update
 
-# Create symlinks
-cd $HOME
-ln -s $OLDPWD/*~*.git~*.gitmodules~*install.zsh~*README.markdown .
+# Symlink to home via stow
+stow ctags
+stow git
+stow hg
+stow tmux
+stow vim
+stow X
+stow zsh
