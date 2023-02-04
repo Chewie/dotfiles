9505fc33877f36445de8c2cb5d1e8698ed829fdc
Kévin Sztern <sztern.kevin@gmail.com>
2023-02-04 12:05:44 +0100

    awesome: use alacritty for terminal

--- xdg/awesome/rc.lua
+++ xdg/awesome/rc.lua
@@ -44,7 +44,7 @@ end
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
