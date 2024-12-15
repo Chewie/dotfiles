da412eda74582cb320a8b887f909db53e9c37f2a
Kévin Sztern <contact@kevinsztern.fr>
2024-12-15 18:51:51 +0100

    nvim: fix telescope trigger keys

--- xdg/nvim/lua/plugins/telescope.lua
+++ xdg/nvim/lua/plugins/telescope.lua
@@ -14,13 +14,55 @@ return {
             "nvim-telescope/telescope-ui-select.nvim",
         },
         keys = {
-            { "<leader>of", require("telescope.builtin").oldfiles, desc = "[OF] Find recently opened files" },
-            { "<leader><space>", require("telescope.builtin").buffers, desc = "[ ] Find existing buffers" },
-            { "<leader>sf", require("telescope.builtin").find_files, desc = "[S]earch [F]iles" },
-            { "<leader>sh", require("telescope.builtin").help_tags, desc = "[S]earch [H]elp" },
-            { "<leader>sw", require("telescope.builtin").grep_string, desc = "[S]earch current [W]ord" },
-            { "<leader>sg", require("telescope.builtin").live_grep, desc = "[S]earch by [G]rep" },
-            { "<leader>sd", require("telescope.builtin").diagnostics, desc = "[S]earch [D]iagnostics" },
+            {
+                "<leader>of",
+                function()
+                    require("telescope.builtin").oldfiles()
+                end,
+                desc = "[OF] Find recently opened files",
+            },
+            {
+                "<leader><space>",
+                function()
+                    require("telescope.builtin").buffers()
+                end,
+                desc = "[ ] Find existing buffers",
+            },
+            {
+                "<leader>sf",
+                function()
+                    require("telescope.builtin").find_files()
+                end,
+                desc = "[S]earch [F]iles",
+            },
+            {
+                "<leader>sh",
+                function()
+                    require("telescope.builtin").help_tags()
+                end,
+                desc = "[S]earch [H]elp",
+            },
+            {
+                "<leader>sw",
+                function()
+                    require("telescope.builtin").grep_string()
+                end,
+                desc = "[S]earch current [W]ord",
+            },
+            {
+                "<leader>sg",
+                function()
+                    require("telescope.builtin").live_grep()
+                end,
+                desc = "[S]earch by [G]rep",
+            },
+            {
+                "<leader>sd",
+                function()
+                    require("telescope.builtin").diagnostics()
+                end,
+                desc = "[S]earch [D]iagnostics",
+            },
             {
                 "<leader>/",
                 function()
