8e051c9107485a5d9107642ea92b95f1e352fcf2
Kévin Sztern <contact@kevinsztern.fr>
2024-12-30 16:30:45 +0100

    nvim: us ruff for python formatting and linting

--- xdg/nvim/lua/plugins/lsp.lua
+++ xdg/nvim/lua/plugins/lsp.lua
@@ -92,9 +92,15 @@ return {
                 ts_ls = {},
                 dagger = {},
                 gopls = {},
-                pyright = {},
+                pyright = {
+                    settings = {
+                        pyright = {
+                            -- Using Ruff's import organizer
+                            disableOrganizeImports = true,
+                        },
+                    },
+                },
                 ruff = {},
-                kcl = {},
                 lua_ls = {
                     -- cmd = {...},
                     -- filetypes { ...},
@@ -110,6 +116,20 @@ return {
                     },
                 },
             }
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if client == nil then
+                        return
+                    end
+                    if client.name == "ruff" then
+                        -- Disable hover in favor of Pyright
+                        client.server_capabilities.hoverProvider = false
+                    end
+                end,
+                desc = "LSP: Disable hover capability from Ruff",
+            })
 
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
